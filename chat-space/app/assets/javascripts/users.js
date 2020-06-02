035d57c85c151f0f549cbe3ee3cccc44f9c2d539
murci-dotnet <mo-gyu@murci.net>
2020-06-02 17:55:14 +0900

    Chat_Incremental_2

--- chat-space/app/assets/javascripts/users.js
+++ chat-space/app/assets/javascripts/users.js
@@ -31,6 +31,7 @@ $(function() {
   }
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
+    let user_ids = [];
     $.ajax({
       type: "GET",
       url: "/users",
