157cbd5deb90db1e1fe9a5360c4aa65a126f16dd
murci-dotnet <mo-gyu@murci.net>
2020-06-04 22:03:47 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,24 +1,53 @@
-# README
+## usersテーブル
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+|Column|Type|Options|
+|------|----|-------|
+|id|integer|null: false|
+|name|string|null: false, unique: true|
+|email|string|null: false, unique: true|
+|password|string|null: false|
 
-Things you may want to cover:
+### Association
+- has_many :groups, through:groups_users
+- has_many :messages
+- has_many :groups_users
 
-* Ruby version
 
-* System dependencies
+## groupsテーブル
 
-* Configuration
+|Column|Type|Options|
+|------|----|-------|
+|id|integer|null: false|
+|name|string|null: false, unique: true|
 
-* Database creation
+### Association
+- has_many :users, through:groups_users
+- has_many :messages
+- has_many :groups_users
 
-* Database initialization
 
-* How to run the test suite
+## messagesテーブル
 
-* Services (job queues, cache servers, search engines, etc.)
+|Column|Type|Options|
+|------|----|-------|
+|id|integer|null: false|
+|user_id|integer|null: false, foreign_key: true|
+|group_id|integer|null: false, foreign_key: true|
+|body|text|
+|image|string|
 
-* Deployment instructions
+### Association
+- belongs_to :group
+- belongs_to :user
 
-* ...
+
+## groups_usersテーブル
+
+|Column|Type|Options|
+|------|----|-------|
+|user_id|integer|null: false, foreign_key: true|
+|group_id|integer|null: false, foreign_key: true|
+
+### Association
+- belongs_to :group
+- belongs_to :user
---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,90 @@
+$(function(){
+
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+       `<div class="chat-main__body__message" data-message-id=${message.id}>
+          <div class="chat-main__body__message__list">
+            ${message.user_name}
+          </div>
+          <div class="chat-main__body__message__time">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="chat-main__body__message-text">
+          <p class="chat-main__body__message-text__content">
+            ${message.body}
+          </p>
+        </div>
+        <img src=${message.image} >
+      </div>`
+      return html;
+    } else {
+      var html =
+       `<div class="chat-main__body__message" data-message-id=${message.id}>
+          <div class="chat-main__body__message__list">
+            ${message.user_name}
+          </div>
+          <div class="chat-main__body__message__time">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="chat-main__body__message-text">
+          <p class="chat-main__body__message-text__content">
+            ${message.body}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__body').append(html);
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました")
+    })
+    .always(function(){
+      $('form')[0].reset();
+    });
+  });
+  var reloadMessages = function(){
+    var last_message_id = $('.chat-main__body__message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+        if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__body').append(insertHTML);
+        $('.chat-main__body').animate({ scrollTop: $('.chat-main__body')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+});
